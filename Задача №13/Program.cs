100efb9ebdba5ac26a97e50df2c633e71cd5b4fb
Aleksandr Basargin <Nommys92@hotmail.com>
2022-11-17 17:03:03 +0300

    Решение задачи №13

--- Задача №13/Program.cs
+++ Задача №13/Program.cs
@@ -3,3 +3,18 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.Clear();
+Console.WriteLine("Введите минимум трёхзначиное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int MaxDigit(int num)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+int result = MaxDigit(number);
+if (number < 100)
+Console.WriteLine("Третьего числа нет");
+else Console.WriteLine(result);
