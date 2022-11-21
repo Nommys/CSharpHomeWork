824c0d7965596740b7d5ede7228158e7f2670670
Aleksandr Basargin <Nommys92@hotmail.com>
2022-11-21 20:18:50 +0300

    Решение задачи №19

--- Задача №19/Program.cs
+++ Задача №19/Program.cs
@@ -7,3 +7,26 @@
 Console.Clear();
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number < 10000)
+{
+    Console.WriteLine("Невозможно проверить, нехватает цифры");
+}
+void Palidrome (int num)
+{
+    if (num > 10000)
+    {
+        int num1 = num / 10000;
+        int num2 = num % 10;
+            if (num1==num2)
+            {
+                num = num / 10;
+                int num3 = (num / 100) % 10;
+                int num4 = num % 10;
+                if (num3==num4)
+                Console.WriteLine("Да");
+            }
+            else Console.WriteLine("Нет");
+    }
+    else Console.WriteLine("Нет");
+}
+Palidrome(number);
