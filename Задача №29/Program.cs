b5195cfe6c786ff325c058e0914811be09b1b3ad
Aleksandr Basargin <Nommys92@hotmail.com>
2022-11-24 00:59:33 +0300

    Добавление условия и решения задачи №29

---
+++ Задача №29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] array = new int[8];
+
+void FindArray(int[] numberarray)
+{
+    int lenght = numberarray.Length;
+    int index = 0;
+    while(index < lenght)
+    {
+        numberarray[index] = new Random().Next (1, 100);
+        index++;
+    }
+}
+
+void PrintArray (int[] numbers)
+{
+    int count = numbers.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write($"{numbers[index]} ");
+        index++;
+    }
+}
+
+FindArray(array);
+PrintArray(array);
