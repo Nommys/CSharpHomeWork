aa839709568ae8ab6c7046d2ca20e7a18fe12ae3
Aleksandr Basargin <Nommys92@hotmail.com>
2022-11-24 15:49:53 +0300

    Добавление решения задачи №27

--- Задача №27/Program.cs
+++ Задача №27/Program.cs
@@ -4,3 +4,33 @@
 // 9012 -> 12
 
 Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)number = -number;
+
+int NumberLen (int num1)
+{
+    int index = 0;
+    while (num1 > 0)
+    {
+        num1 = num1 / 10;
+        index++;
+    }return index;
+}
+
+int len = NumberLen(number);
+
+int SumDigit (int num)
+{
+    int sum = default;
+    for (int i = 0; i < len; i++)
+   {
+        sum = sum + num % 10;
+        num = num / 10;
+    }return sum;
+}
+
+int result = SumDigit(number);
+
+Console.WriteLine(result);
