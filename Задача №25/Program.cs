8ca53c348f3b988baf20745a0e2a1c14a7e28dcd
Aleksandr Basargin <Nommys92@hotmail.com>
2022-11-24 17:10:37 +0300

    Добавления условия и решения задачи №25

---
+++ Задача №25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральную степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+if (degree < 0)
+{
+    Console.WriteLine("Степень не должна быть отрицательной");
+}
+else
+{ int DegreeNumber (int sum)
+{
+    for (int i = 1; i < degree; i++)
+    {
+        sum = sum * number;
+    }
+    return sum;
+}
+int result = DegreeNumber(number);
+Console.WriteLine(result);
+}
