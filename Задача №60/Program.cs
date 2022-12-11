63d4ba8738dfd3df05164bbfcce1903c0504c98d
Aleksandr Basargin <Nommys92@hotmail.com>
2022-12-11 13:42:48 +0300

    Добавление условия и решения  задачи №60

---
+++ Задача №60/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача №60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+int[,,] matrix3D = new int [2,2,2];
+
+void CreatedMatrix3D(int[,,] matrix3D)
+{
+    int[] temp = new int[matrix3D.GetLength(0) * matrix3D.GetLength(1) * matrix3D.GetLength(2)];
+    int  number = default;
+    Random rnd = new Random();
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j <  i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+            }
+        }
+    }
+    int count = default; 
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+            matrix3D[i, j, k] = temp[count];
+            count++;
+            }
+        }
+    }
+}
+void PrintMatrix3DIndex(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"|{matrix3D[i, j, k], 2} ({i},{j},{k})| ");
+            }
+        }
+    }
+}
+CreatedMatrix3D(matrix3D);
+PrintMatrix3DIndex(matrix3D);
